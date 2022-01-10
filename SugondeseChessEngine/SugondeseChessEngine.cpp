c1b1f4f99a0358db17f979aa6ef8926c9db7355b
UrsaMajorrr <56418085+UrsaMajorrr@users.noreply.github.com>
2022-01-10 17:54:06 -0500

    first commit ever -Kade Carlson

--- SugondeseChessEngine/SugondeseChessEngine.cpp
+++ SugondeseChessEngine/SugondeseChessEngine.cpp
@@ -1,9 +1,18 @@
-
+/*
+Sugondese Chess Engine; version 1.0
+Authors: Kade Carlson, Noah Muthler, Quintin Nelson
+Contact Info:
+kkc5441@psu.edu
+nsm5313@psu.edu
+qcn101@psu.edu
+Start Date: 1/10/2022
+*/
 
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    cout << "Hello World!\n";
 }
 
