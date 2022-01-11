e3c85f63d4d925843013212c3c045c1d2ca9ec86
UrsaMajorrr <56418085+UrsaMajorrr@users.noreply.github.com>
2022-01-10 19:03:55 -0500

    added a lot to the board

--- SugondeseChessEngine/SugondeseChessEngine.cpp
+++ SugondeseChessEngine/SugondeseChessEngine.cpp
@@ -9,6 +9,10 @@ Start Date: 1/10/2022
 */
 
 #include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <string>
+#include <Windows.h>
 #include "Piece.cpp"
 
 int main()
@@ -20,5 +24,16 @@ int main()
     rook.value = 3;
     std::cout << knight.getPieceValue() << "\n";
     std::cout << knight.getPieceName();
+
+
+    /*Code below is a reference to how we may want to show our board. 
+    However, it was taken from another project so might have to figure the rest out on our own. just a good start*/
+
+    /*HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    std::cout << "|";
+    SetConsoleTextAttribute(hConsole, 114);
+    std::cout << "    a  b  c  d  e  f  g  h    ";
+    SetConsoleTextAttribute(hConsole, 15);
+    std::cout << "|" << std::endl;*/
 }
 
