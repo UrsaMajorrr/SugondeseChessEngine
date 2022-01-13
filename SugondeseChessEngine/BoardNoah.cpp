3bbf42655ff62491469d14734fea63b8c356ce3b
Noah Muthler <noahmuthler@gmail.com>
2022-01-13 10:20:21 -0500

    board

--- SugondeseChessEngine/BoardNoah.cpp
+++ SugondeseChessEngine/BoardNoah.cpp
@@ -15,43 +15,29 @@ Start Date: 1/10/2022
 #include <Windows.h>
 using namespace std;
 
+string spaced( string str )
+{
+   if ( str == "" ) return str;
 
-/*
-int main() {
-    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-    std::cout << "|";
-    SetConsoleTextAttribute(hConsole, 114);
-    std::cout << "    a  b  c  d  e  f  g  h    ";
-    SetConsoleTextAttribute(hConsole, 15);
-    std::cout << "|" << std::endl;
+   string result( 2 * str.size() - 1, ' ' );
+   for ( int i = 0, j = 0; i < str.size(); i++, j+=2 ) result[j] = str[i];
+   return result;
 }
-*/
-/*
-int main() {
-    char board[] = "RNBQKBNRPPPPPPPP________________________________pppppppprnbqkbnr";
-    char temp[8];
-    for(int i=0; i<8; i++) {
-        for(int j=0; j < 8; j++) {
-            temp[j] = board[j*8 + i];
-        }
-        std::cout << temp;
-        std::cout << "\n";
-    }
-}
-*/
 
-int main() {
-    //namespace rs = ranges;
-    //namespace rv = ranges::views;
-    string input = "RNBQKBNRPPPPPPPP________________________________pppppppprnbqkbnr";
-    //auto board = input | rv::intersperse(' ') | rs::to<std::string>;
+void printBoard(string input) {
+    string board = spaced(input);
     string end = "\n";
-    cout << board.substr(0,8) + end;
-    cout << board.substr(8,8) + end;
-    cout << board.substr(16,8) + end;
-    cout << board.substr(24,8) + end;
-    cout << board.substr(32,8) + end;
-    cout << board.substr(40,8) + end;
-    cout << board.substr(48,8) + end;
-    cout << board.substr(56,8) + end;
+    cout << board.substr(0,16) + end;
+    cout << board.substr(16,16) + end;
+    cout << board.substr(32,16) + end;
+    cout << board.substr(48,16) + end;
+    cout << board.substr(64,16) + end;
+    cout << board.substr(80,16) + end;
+    cout << board.substr(96,16) + end;
+    cout << board.substr(112,16) + end;
+}
+
+int main() {
+    string input = "RNBQKBNRPPPPPPPP________________________________pppppppprnbqkbnr";
+    printBoard(input);
 }
