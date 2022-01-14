73b07c05bfdb2ed089f6d040a23e3e0f441330db
Kevin Dong <kfd5270@psu.edu>
2022-01-13 21:46:40 -0500

    Update Rook.cpp

--- SugondeseChessEngine/Pieces/Rook.cpp
+++ SugondeseChessEngine/Pieces/Rook.cpp
@@ -1,3 +1,14 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+//  File           : Rook.cpp
+//  Description    : This class holds all the functions and variables of the rook
+//					 This is also the child class of Piece
+//
+//  Authors        : Kade Carlson, Noah Muthler, Quintin Nelson, Kevin Dong
+//  Last Modified  : 1/13/2022 9:13 PM EST
+//
+
+//includes
 #include "Piece.cpp"
 #include <iostream>
 #include <string>
@@ -8,6 +19,9 @@ using namespace std;
 
 class Rook : public Piece {
 public:
+    //constructor
+    Rook(int postion, COLOR color, bool isFirstMove) : Piece(5, postion, PieceType::ROOK, color, isFirstMove){};
+
 	bool isLegalMove() {
 		return true;
 	}
