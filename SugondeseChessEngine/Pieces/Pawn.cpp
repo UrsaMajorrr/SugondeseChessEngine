49d5de28fd1b32d7a1455b89085777a73a4d16fa
Kevin Dong <kfd5270@psu.edu>
2022-01-13 21:41:05 -0500

    Update Pawn.cpp

--- SugondeseChessEngine/Pieces/Pawn.cpp
+++ SugondeseChessEngine/Pieces/Pawn.cpp
@@ -3,12 +3,16 @@
 #include <string>
 using namespace std;
 
-
-
-class Pawn : public Piece {
+class Pawn : public Piece
+{
 public:
-	bool isLegalMove() {
-		std::cout << "This move may or may not be legal";
-		return true;
-	}
+    //Constructor
+    Pawn( int postion, COLOR color, bool isFirstMove) : Piece(1, postion, PieceType::PAWN, color, isFirstMove){
+    };
+    
+    bool isLegalMove()
+    {
+        std::cout << "This move may or may not be legal";
+        return true;
+    }
 };
