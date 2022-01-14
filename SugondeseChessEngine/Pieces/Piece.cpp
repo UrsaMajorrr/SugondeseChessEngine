a65a8dae96d06acab852c27545de6a29d13eb24e
Kevin Dong <kfd5270@psu.edu>
2022-01-13 20:18:37 -0500

    Create Piece.cpp

---
+++ SugondeseChessEngine/Pieces/Piece.cpp
@@ -0,0 +1,36 @@
+#include <string>
+#include <iostream>
+
+
+
+/*
+Sugondese Chess Engine; version 1.0
+Authors: Kade Carlson, Noah Muthler, Quintin Nelson, Kevin Dong
+Contact Info:
+kkc5441@psu.edu
+nsm5313@psu.edu
+qcn101@psu.edu
+kfd5270@psu.edu
+Start Date: 1/10/2022
+
+Description: This class holds all the functions and variables that pieces have in common.
+*/
+
+class Piece {
+public:
+	int value;
+    std::string name;
+
+	int getPieceValue() {
+		return value;
+	}
+
+	std::string getPieceName() {
+		return name;
+	}
+
+	bool isLegalMove() {
+		std::cout << "Move is legal";
+		return true;
+	}
+};
